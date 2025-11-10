3540746f4dd6fda6de7271bfc9e652951e9a47fc
yuchen-wei3667 <weiyu19one@gmail.com>
2025-11-10 15:43:52 -0500

    ADD: Added `EnhancedEchoTop` colormap and EchoTop18 processing to `gui.transform`

--- src/EdgeWARN/gui/transform/config.py
+++ src/EdgeWARN/gui/transform/config.py
@@ -6,5 +6,11 @@ file_list = [
         "colormap_key": "NWS_Reflectivity",
         "filepath": MRMS_COMPOSITE_DIR,
         "outdir": GUI_COMPOSITE_DIR
+    },
+    {
+        "name": "MRMS_EchoTop18",
+        "colormap_key": "EnhancedEchoTop",
+        "filepath": MRMS_ECHOTOP18_DIR,
+        "outdir": GUI_ECHOTOP18_DIR
     }
 ]
