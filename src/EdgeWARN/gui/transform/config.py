53c376451ddaed933fcb8bcf7445d9a1c3eb3bed
yuchen-wei3667 <weiyu19one@gmail.com>
2025-11-10 15:47:15 -0500

    ADD: Added `MRMS_RALA` png generation to GUI transform pipeline

--- src/EdgeWARN/gui/transform/config.py
+++ src/EdgeWARN/gui/transform/config.py
@@ -12,5 +12,11 @@ file_list = [
         "colormap_key": "EnhancedEchoTop",
         "filepath": MRMS_ECHOTOP18_DIR,
         "outdir": GUI_ECHOTOP18_DIR
+    },
+    {
+        "name": "MRMS_ReflectivityAtLowestAltitude",
+        "colormap_key": "NWS_Reflectivity",
+        "filepath": MRMS_RALA_DIR,
+        "outdir": GUI_RALA_DIR
     }
 ]
